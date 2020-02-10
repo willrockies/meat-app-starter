61bef68157d69dee6aa180c9d7d2d73efaaaa3ee
Wilson Alves da Silva <w.alves.da.silva@avanade.com>
2020-02-10 16:55:01 -0300

    criando componente restaurante

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,18 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
-import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, RestaurantsComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent
+  ],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
