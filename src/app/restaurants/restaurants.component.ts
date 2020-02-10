05575fec51ea6adfb390e938847ea5768593b21e
Wilson Alves da Silva <w.alves.da.silva@avanade.com>
2020-02-10 14:33:22 -0300

    rotas para o menu

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "mt-restaurants",
+  templateUrl: "./restaurants.component.html"
+})
+export class RestaurantsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
