637c3622a85e2990c373c266e3d5d940b522d997
impactaope2020 <wilson.silva@aluno.faculdadeimpacta.com.br>
2020-02-13 16:22:05 -0300

    servico http

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -13,9 +13,9 @@ import { ErrorHandler } from "../app.error-handler";
 export class RestaurantsService {
   constructor(private http: Http) {}
 
-  restaurants(): Observable<Restaurant[]> {
+  restaurantsById(id: string): Observable<Restaurant> {
     return this.http
-      .get(`${MEAT_API}/restaurants`)
+      .get(`${MEAT_API}/restaurants/${id}`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
   }
