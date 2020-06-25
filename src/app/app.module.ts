af3cb09d2c8c22c5bab49ee6b26ab012a4c2390d
willrockies <wlsn.ads@gmail.com>
2020-06-24 22:53:32 -0300

    formularios e componentes personalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, LOCALE_ID } from "@angular/core";
 import { HttpModule } from "@angular/http";
 import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 import { ROUTES } from "./app.routes";
 
@@ -19,6 +20,11 @@ import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
 import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
+import { OrderService } from "./order/order.service";
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 
 @NgModule({
   declarations: [
@@ -33,14 +39,19 @@ import { OrderComponent } from './order/order.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent,
+    RadioComponent,
+    OrderItemsComponent,
+    DeliveryCostsComponent
   ],
-  imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
+  imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES), FormsModule],
   providers: [
     RestaurantsService,
     ShoppingCartService,
+    OrderService,
     { provide: LOCALE_ID, useValue: "pt-BR" }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,14 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { OrderService } from './order.service';
+import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+
+  paymentOptions: RadioOption[] = [
+    { label: 'Dinheiro', value: 'MON' },
+    { label: 'Cartão de Débito', value: 'DEB' },
+    { label: 'Cartão Refeição', value: 'REF' }
+  ]
+
+  delivery: number = 8
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
   }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue()
+  }
+
+  cartItems(): CartItem[] {
+    return this.orderService.cartItems();
+  }
+  increaseQty(item: CartItem) {
+    this.orderService.increaseQty(item);
+  }
+  decreaseQty(item: CartItem) {
+    this.orderService.decreaseQty(item);
+  }
+  remove(item: CartItem) {
+    this.orderService.remove(item);
+  }
+
 
 }
